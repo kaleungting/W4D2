23a62e664d596021650987736626de52f3a01947
App Academy Student <email@email.com>
2020-02-11 14:12:24 -0500

    Implemented error handling exercises

---
+++ error_handling/super_useful.rb
@@ -0,0 +1,57 @@
+# PHASE 2
+def convert_to_int(str)
+  begin
+    Integer(str)
+  rescue ArgumentError
+    nil
+  end
+end
+
+# PHASE 3
+FRUITS = ["apple", "banana", "orange"]
+
+def reaction(maybe_fruit)
+  if FRUITS.include? maybe_fruit
+    puts "OMG, thanks so much for the #{maybe_fruit}!"
+  elsif maybe_fruit == "coffee"
+    raise StandardError.new("coffee")
+  else
+    raise StandardError.new("feed me fruit, pls")
+  end 
+end
+
+def feed_me_a_fruit
+  puts "Hello, I am a friendly monster. :)"
+  begin
+    puts "Feed me a fruit! (Enter the name of a fruit:)"
+    maybe_fruit = gets.chomp
+    reaction(maybe_fruit) 
+  rescue StandardError => e
+    retry if e.message == "coffee"
+  end
+end  
+
+# PHASE 4
+class BestFriend
+  def initialize(name, yrs_known, fav_pastime)
+    raise "We have not been friends for at least 5 years :(" if yrs_known < 5
+    raise "I need both your name and your favorite pastime!" if name.length <= 0 || fav_pastime.length <= 0
+    @name = name
+    @yrs_known = yrs_known
+    @fav_pastime = fav_pastime
+  end
+
+  def talk_about_friendship
+    puts "Wowza, we've been friends for #{@yrs_known}. Let's be friends for another #{1000 * @yrs_known}."
+  end
+
+  def do_friendstuff
+    puts "Hey bestie, let's go #{@fav_pastime}. Wait, why don't you choose. 😄"
+  end
+
+  def give_friendship_bracelet
+    puts "Hey bestie, I made you a friendship bracelet. It says my name, #{@name}, so you never forget me." 
+  end
+end
+
+
