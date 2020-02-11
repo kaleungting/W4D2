f5df5c82339cd24789aad3eb12673642a07f1346
App Academy Student <email@email.com>
2020-02-11 16:32:04 -0500

    Implemented starting version of Board class & shell Piece class

---
+++ chess_part1/board.rb
@@ -0,0 +1,44 @@
+require_relative "piece"
+
+class Board
+  attr_reader :rows
+  def initialize
+    @rows = Array.new(8) { Array.new(8) }
+    @sentinel = nil
+    (0...rows.length).each do |r|
+      (0...rows.length).each do |c|
+        if r == 1 || r == 0
+          @rows[r][c] = Piece.new(:black, self, [r, c])
+        elsif r == 7 || r == 6
+          @rows[r][c] = Piece.new(:white, self, [r, c])
+        else
+          @rows[r][c] = @sentinel
+        end
+      end
+    end
+  end
+
+  def [](pos)
+    r, c = pos
+    raise "Invalid position coordinates" if r >= 8 || r < 0 || c >= 8 || c < 0
+    rows[r][c]
+  end
+
+  def []=(pos, piece)
+    r, c = pos
+    raise "Invalid position coordinates" if r >= 8 || r < 0 || c >= 8 || c < 0
+    rows[r][c] = piece
+  end
+
+  def move_piece(start_pos, end_pos)
+    raise "Invalid starting position" if self[start_pos] == sentinel
+    raise "Cannot move to ending positon" if self[end_pos] != sentinel && self[end_pos].color == self[start_pos].color
+
+    self[end_pos] = self[start_pos]
+    self[end_pos].pos = end_pos
+    self[start_pos] = sentinel
+  end
+
+  private
+  attr_reader :sentinel
+end
