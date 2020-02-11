03195dcd1a56ced60e8d377363f1d340f6debd66
App Academy Student <email@email.com>
2020-02-11 17:32:03 -0500

    Started implementing Pieces

--- chess_part1/piece.rb
+++ chess_part1/piece.rb
@@ -1,3 +1,6 @@
+require_relative "slideable"
+require_relative "steppable"
+
 class Piece
   attr_reader :color, :board
   attr_accessor :pos
@@ -26,4 +29,111 @@ class Piece
   
   end
 
+end
+
+class Rook < Piece
+  include Slideable
+
+  def symbol
+    :r
+  end
+
+  protected
+  def move_dirs
+
+  end
+end
+
+class Bishop < Piece
+  include Slideable
+
+  def symbol
+    :b
+  end
+
+  protected
+  def move_dirs
+    
+  end
+end
+
+class Queen < Piece
+  include Slideable
+
+  def symbol
+    :q
+  end
+
+  protected
+  def move_dirs
+    
+  end
+end
+
+class Knight < Piece
+  include Steppable
+
+  def symbol
+    :k
+  end
+
+  protected
+  def move_dirs
+    
+  end
+end
+
+class King < Piece
+  include Steppable
+
+  def symbol
+    :K
+  end
+
+  protected
+  def move_dirs
+    
+  end
+end
+
+class Pawn < Piece
+  def symbol
+    :p
+  end
+
+  def move_dirs
+    
+  end
+
+  private
+  def at_start_row?
+
+  end
+
+  def forward_dir
+
+  end
+
+  def forward_steps
+
+  end
+
+  def side_attacks
+
+  end
+end
+
+class NullPiece < Piece
+  include Singleton
+  def initialize
+
+  end
+
+  def symbol
+    :_
+  end
+
+  def moves
+    []
+  end
 end
---
+++ chess_part1/slideable.rb
@@ -0,0 +1,26 @@
+module Slideable
+  def horizontal_dirs
+
+  end
+
+  def diagonal_dirs 
+  
+  end
+
+  # uses move_dirs
+  def moves
+
+  end
+
+  private
+  HORIZONTAL_DIRS = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+  DIAGONAL_DIRS = [[1, 1], [-1, -1], [-1, 1], [1, -1]]
+
+  def move_dirs
+
+  end
+
+  def grow_unblocked_moves_in_dir(dx, dy)
+
+  end
+end
